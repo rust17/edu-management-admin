3aa7a2919a1bd206f8997368a87012897138a905
circle33 <newnash@163.com>
2025-01-16 08:53:39 +0000

    debug

--- docker/init.sh
+++ docker/init.sh
@@ -3,5 +3,13 @@
 # 启动 Nginx
 nginx -g "daemon on; master_process on;"
 
+# debug
+echo "APP_ENV=local
+APP_DEBUG=true
+" > .env
+
+# 查看有没有 .env 文件
+cat .env
+
 # 启动 PHP-FPM
 php-fpm --fpm-config /usr/local/etc/php-fpm.d/www.conf
